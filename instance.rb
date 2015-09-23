2ddfc7f8789601850f433eeea593aa4d146684f5
ihiteshri12 <hitiramteke93@gmail.com>
2015-09-24 02:33:17 +0530

    instance

---
+++ instance.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+class Customer
+ def initialize(id, name, addr)
+@cust_id=id
+@cust_name=name
+@cust_addr=addr
+end
+def display_details()
+puts "Customer id #@cust_id"
+puts "Customer name #@cust_name"
+puts "Customer address is #@cust_addr"
+ end
+end
+
+cust1=Customer.new("1", "Hiti", "Behind total mall")
+cust2=Customer.new("2", "Shruti", "Near empress mall")
+
+cust1.Display_details()
+cust2.Display_details()
+
+     
